2c305ed27e8fb3ab03e1d27b2be3657e4c482e34
Oussama <oussama.ouerd@gmail.com>
2021-06-25 14:27:36 +0100

    logo removed

--- src/components/Layout/NavBar/NavBar.js
+++ src/components/Layout/NavBar/NavBar.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../../assets/img/covid-19-corona.png';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav className="navbar navbar-light bg-light mb-3">
       <Link className="navbar-brand" to="/">
-        <img
-          src={logo}
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-          alt=""
-        />
         Covid-19 Tracker
       </Link>
     </nav>
